bb54599627855b9f814a15b24f46f0e951947ec4
17135405 <FackSudir021>
2022-10-08 18:03:56 +0300

    CBNII-221

--- src/main/java/api/PlatformServiceFactoryCreator.java
+++ src/main/java/api/PlatformServiceFactoryCreator.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.dbutils.QueryRunner;
 
+import javax.sql.DataSource;
 import java.util.Properties;
 
 public class PlatformServiceFactoryCreator {
@@ -14,11 +15,17 @@ public class PlatformServiceFactoryCreator {
         this.classLoader = classLoader;
     }
 
+    @Deprecated
     public PlatformServiceFactory create(PlatformDatabaseConnection connection, String schema) throws ClassNotFoundException {
         Properties props = connection.toProperties();
         HikariConfig config = new HikariConfig(props);
         HikariDataSource dataSource = new HikariDataSource(config);
+        return create(dataSource, schema);
+    }
+
+    public PlatformServiceFactory create(DataSource dataSource, String schema) throws ClassNotFoundException {
         QueryRunner queryRunner = new QueryRunner(dataSource);
         return new DbPlatformServiceFactory(queryRunner, schema, classLoader);
     }
+
 }
